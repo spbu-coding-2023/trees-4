cf5368680325f393a467dee0bf8b703777258084
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-23 15:52:18 +0300

    feat: add RBNode

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -1,4 +1,11 @@
 package xddcc.nodes
 
-class RBNode {
+class RBNode<K: Comparable<K>> (
+    var value: Any,
+    val key: K,
+    var right: RBNode<K>? = null,
+    var left: RBNode<K>? = null,
+) {
+    enum class Color {RED, BLACK}
+    var color = Color.RED
 }
