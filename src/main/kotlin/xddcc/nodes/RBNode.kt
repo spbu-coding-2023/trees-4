86b461dfd730ce9d4e36371503d7cff634c17c85
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-24 22:43:23 +0300

    fix!: simplified version of Node (without value)

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -1,19 +1,14 @@
 package xddcc.nodes
 
-class RBNode<K: Comparable<K>, V> (
-    var value: V,
+class RBNode<K: Comparable<K>>(
     val key: K,
-    var right: RBNode<K, *>? = null,
-    var left: RBNode<K, *>? = null,
-): Comparable<RBNode<K, *>> {
+    var right: RBNode<K>? = null,
+    var left: RBNode<K>? = null,
+): Comparable<RBNode<K>> {
     enum class Color {RED, BLACK}
     var color = Color.RED
 
-    override fun compareTo(other: RBNode<K, *>): Int {
-        return when {
-            key > other.key -> 1
-            key < other.key -> -1
-            else -> 0
-        }
+    override fun compareTo(other: RBNode<K>): Int {
+        return key.compareTo(other.key)
     }
 }
