0de843e3ee341a8f988910e2fb1963c9e2945b70
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-24 17:17:56 +0300

    feat: add fun compareTo(), add generic type V for value

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -1,11 +1,19 @@
 package xddcc.nodes
 
-class RBNode<K: Comparable<K>> (
-    var value: Any,
+class RBNode<K: Comparable<K>, V> (
+    var value: V,
     val key: K,
-    var right: RBNode<K>? = null,
-    var left: RBNode<K>? = null,
-) {
+    var right: RBNode<K, *>? = null,
+    var left: RBNode<K, *>? = null,
+): Comparable<RBNode<K, *>> {
     enum class Color {RED, BLACK}
     var color = Color.RED
+
+    override fun compareTo(other: RBNode<K, *>): Int {
+        return when {
+            key > other.key -> 1
+            key < other.key -> -1
+            else -> 0
+        }
+    }
 }
