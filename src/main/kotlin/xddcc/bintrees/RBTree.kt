8f7bb9f60254ff1cfa04b249dda6221bc4c7c4e5
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-24 22:45:47 +0300

    feat: add simple version of fun add()

--- src/main/kotlin/xddcc/bintrees/RBTree.kt
+++ src/main/kotlin/xddcc/bintrees/RBTree.kt
@@ -1,4 +1,42 @@
 package xddcc.bintrees
 
-class RBTree {
+import xddcc.nodes.RBNode
+
+class RBTree<K: Comparable<K>> {
+    private var root: RBNode<K>? = null
+
+    fun add(key: K): Boolean {
+        val addNode = RBNode(key)
+        if (root == null) {
+            root = addNode
+            return true
+        }
+
+        var curNode = root
+        while (curNode != null) {
+            when {
+                addNode > curNode -> {
+                    if (curNode.right == null) {
+                        curNode.right = addNode
+                        return true
+                    } else {
+                        curNode = curNode.right
+                    }
+                }
+                addNode < curNode -> {
+                    if (curNode.left == null) {
+                        curNode.left = addNode
+                        return true
+                    } else {
+                        curNode = curNode.left
+                    }
+                }
+                addNode == curNode -> return false
+            }
+        }
+
+        return false
+    }
+
+
 }
