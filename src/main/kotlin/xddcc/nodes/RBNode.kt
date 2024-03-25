847533457bc731292e151ac88ca83cf9a8e5f208
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-25 15:35:47 +0300

    fix: change field color: Color to red: Boolean

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -2,13 +2,9 @@ package xddcc.nodes
 
 class RBNode<K: Comparable<K>>(
     val key: K,
+    var red: Boolean = true,
     var right: RBNode<K>? = null,
     var left: RBNode<K>? = null,
 ): Comparable<RBNode<K>> {
-    enum class Color {RED, BLACK}
-    var color = Color.RED
-
-    override fun compareTo(other: RBNode<K>): Int {
-        return key.compareTo(other.key)
-    }
+    override fun compareTo(other: RBNode<K>) = this.key.compareTo(other.key)
 }
