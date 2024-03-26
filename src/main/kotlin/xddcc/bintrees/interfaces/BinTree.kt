4bbc6677eae4912f4b0046066cc8451f7ffb3e75
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-26 20:22:39 +0300

    feat: BinTree interface BinTree implements fun iterator(), add(), remove(), find(), max(), min(), root(), clear(), size()

---
+++ src/main/kotlin/xddcc/bintrees/interfaces/BinTree.kt
@@ -0,0 +1,46 @@
+package xddcc.bintrees.interfaces
+
+import xddcc.nodes.TreeNode
+
+interface BinTree<K: Comparable<K>, V, Node_T: TreeNode<K, V, Node_T>>: Iterable<Pair<K, V>> {
+    var root: Node_T?
+    var size: Int
+
+    override fun iterator(): Iterator<Pair<K, V>> = TreeIterator(root)
+
+    fun add(key: K, value: V): Pair<K, V>?
+
+    fun remove(key: K): Pair<K, V>?
+
+    fun find(key: K): Pair<K, V>? {
+        var curNode = root
+        while (curNode != null)
+            curNode = when {
+                key > curNode.key -> curNode.right
+                key < curNode.key -> curNode.left
+                else -> return Pair(key, curNode.value)
+            }
+        return null
+    }
+
+    fun max(): Pair<K, V>? {
+        var curNode = root
+        while (curNode?.right != null)
+            curNode = curNode.right
+        return if (curNode != null) Pair(curNode.key, curNode.value) else null
+    }
+
+
+    fun min(): Pair<K, V>? {
+        var curNode = root
+        while (curNode?.left != null)
+            curNode = curNode.left
+        return if (curNode != null) Pair(curNode.key, curNode.value) else null
+    }
+
+    fun root(): Pair<K, V>? = root?.let { Pair(it.key, it.value) }
+
+    fun clear() { root?.let { root = null } }
+
+    fun size(): Int = size
+}
