5c8a0f47eb08ff192c4a60b3e97963ed8c2c2952
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-26 17:19:08 +0300

    feat: add fun attach()

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -8,4 +8,13 @@ class RBNode<K: Comparable<K>, V>(
     var left: RBNode<K, V>? = null,
 ): Comparable<RBNode<K, *>> {
     override fun compareTo(other: RBNode<K, *>) = this.key.compareTo(other.key)
+
+    fun attach(node: RBNode<K, V>): Boolean {
+        when {
+            this > node -> this.left = node
+            this < node -> this.right = node
+            else -> return false
+        }
+        return true
+    }
 }
