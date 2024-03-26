a563e67c353673acb20ad3f5c31bb53e6b63327e
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-26 21:58:55 +0300

    feat: add fun attach() and moveOn()

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -6,4 +6,20 @@ class RBNode<K: Comparable<K>, V>(
     right: RBNode<K, V>? = null,
     left: RBNode<K, V>? = null,
     var isRed: Boolean = true,
-): TreeNode<K, V, RBNode<K, V>>(key, value, right, left)
+): TreeNode<K, V, RBNode<K, V>>(key, value, right, left) {
+    override fun attach(node: RBNode<K, V>?): Boolean {
+        if (node == null) return false
+        when {
+            this > node -> left = node
+            this < node -> right = node
+            else -> return false
+        }
+        return true
+    }
+
+    override fun moveOn(otherKey: K) = when {
+        key > otherKey -> left
+        key < otherKey -> right
+        else -> this
+    }
+}
