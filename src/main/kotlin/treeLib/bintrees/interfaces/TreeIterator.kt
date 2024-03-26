0cfa29e0a0b795e4791de87a1e0b7e666e557d7b
Sofya Grishkova <sofagriskova1@gmail.com>
2024-03-26 21:06:28 +0300

    feat: support for TreeIterator (avl)

---
+++ src/main/kotlin/treeLib/bintrees/interfaces/TreeIterator.kt
@@ -0,0 +1,25 @@
+package treeLib.bintrees.interfaces
+
+import treeLib.nodes.TreeNode
+
+class TreeIterator<K: Comparable<K>, V, Node_T: TreeNode<K, V, Node_T>>(private val root: Node_T?): Iterator<Pair<K, V>>{
+	private val stack: ArrayDeque<Node_T> = ArrayDeque()
+	init {
+		root?.let { treeToStack(root) }
+	}
+
+	private fun treeToStack(curNode: Node_T) {
+		val leftNode = curNode.left
+		val rightNode = curNode.right
+		leftNode?.let { treeToStack(leftNode) }
+		stack.add(curNode)
+		rightNode?.let { treeToStack(rightNode) }
+	}
+
+	override fun hasNext() = stack.isNotEmpty()
+
+	override fun next(): Pair<K, V> {
+		val node = stack.removeFirst()
+		return Pair(node.key, node.value)
+	}
+}
