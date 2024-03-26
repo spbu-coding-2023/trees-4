89a6c64c897ff2ca677f846c50e1197bbbc087f8
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-26 20:18:04 +0300

    fix: RBNode implements TreeNode now

--- src/main/kotlin/xddcc/nodes/RBNode.kt
+++ src/main/kotlin/xddcc/nodes/RBNode.kt
@@ -1,20 +1,9 @@
 package xddcc.nodes
 
 class RBNode<K: Comparable<K>, V>(
-    val key: K,
-    val value: V,
+    key: K,
+    value: V,
+    right: RBNode<K, V>? = null,
+    left: RBNode<K, V>? = null,
     var isRed: Boolean = true,
-    var right: RBNode<K, V>? = null,
-    var left: RBNode<K, V>? = null,
-): Comparable<RBNode<K, *>> {
-    override fun compareTo(other: RBNode<K, *>) = this.key.compareTo(other.key)
-
-    fun attach(node: RBNode<K, V>): Boolean {
-        when {
-            this > node -> this.left = node
-            this < node -> this.right = node
-            else -> return false
-        }
-        return true
-    }
-}
+): TreeNode<K, V, RBNode<K, V>>(key, value, right, left)
