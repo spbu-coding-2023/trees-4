ed77c40e0661bef3868fd0944da6db526ab0dbd2
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-30 19:40:25 +0300

    fix: expanded types for node in rotation funs

--- src/main/kotlin/treeLib/bintrees/interfaces/TreeBalancer.kt
+++ src/main/kotlin/treeLib/bintrees/interfaces/TreeBalancer.kt
@@ -10,7 +10,9 @@ interface TreeBalancer<K : Comparable<K>, V, Node_T : TreeNode<K, V, Node_T>> {
 
 	fun balancerRemove(treeBranch: ArrayDeque<RBNode<K, V>>)
 
-	fun rotateRight(node: RBNode<K, V>, parent: RBNode<K, V>?) {
+	fun rotateRight(node: RBNode<K, V>?, parent: RBNode<K, V>?) {
+		if (node == null) return
+
 		val nodeLeft = node.left
 		node.left = nodeLeft?.right
 		nodeLeft?.right = node
@@ -21,7 +23,9 @@ interface TreeBalancer<K : Comparable<K>, V, Node_T : TreeNode<K, V, Node_T>> {
 			root = nodeLeft
 	}
 
-	fun rotateLeft(node: RBNode<K, V>, parent: RBNode<K, V>?) {
+	fun rotateLeft(node: RBNode<K, V>?, parent: RBNode<K, V>?) {
+		if (node == null) return
+
 		val nodeRight = node.right
 		node.right = nodeRight?.left
 		nodeRight?.left = node
