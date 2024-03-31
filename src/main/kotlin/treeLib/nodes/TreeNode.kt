fcce19883ef3ce77ca4e226bcc689619a5742d2f
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-03-31 15:10:39 +0300

    refactor: rewrite fun equals() (again)

--- src/main/kotlin/treeLib/nodes/TreeNode.kt
+++ src/main/kotlin/treeLib/nodes/TreeNode.kt
@@ -12,8 +12,7 @@ abstract class TreeNode<K : Comparable<K>, V, Node_T : TreeNode<K, V, Node_T>>(
 	override fun hashCode() = Pair(key, value).hashCode()
 
 	override fun equals(other: Any?): Boolean {
-		val equals = (other as? Node_T != null) && (key == other.key) && (value == other.value)
-		return equals
+		return (other as? Node_T != null) && (key == other.key) && (value == other.value)
 	}
 
 	override fun toString() = Pair(key, value).toString()
